fd10106c3839e14c7501f87ba0b2c558f12a989f
wilmer_araujo <wilmeraraujo@emssanar.org.co>
2023-05-15 17:43:13 -0500

    paso a punto 9

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Loading } from "./loading";
 
+const SECURITY_CODE='paradigma';
+
 class ClassState extends React.Component{ 
     constructor(props){
         super(props);
         this.state = {
-            error : true,
+            value : '',
+            error : false,
             loading : false, 
         };
     }
@@ -22,26 +25,39 @@ class ClassState extends React.Component{
         if(!!this.state.loading){
             setTimeout( () => {
                 console.log("Haciendo la validacion");
-                this.setState({loading : false});
+
+                if(SECURITY_CODE === this.state.value){
+                    this.setState({error : false, loading : false});
+                }else{
+                    this.setState({error : true, loading : false});
+                }
                 console.log("Terminando la validacion");
-            },3000); 
+            },1000); 
         }
     }
     
     render(){
+        //const { error,loading,value} = this.state;
+
         return(
             <div>
                 <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escriba el codigo de seguridad.</p>
                 
-                {this.state.error && (
+                {(this.state.error && !this.state.loading) && (
                     <p>Error: el codigo es incorrecto</p>
                 )}
 
                 {this.state.loading && (
                     <Loading/>
                 )}
-                <input placeholder="Código de seguridad"/>
+                <input 
+                    placeholder="Código de seguridad"
+                    value = {this.state.value}
+                    onChange={(event) =>{
+                        this.setState({value:event.target.value});
+                    }}
+                />
                 <button onClick={() => 
                     //</div>this.setState({error:!this.state.error})
                     //this.setState(prevState => ({error: !prevState.error}))
